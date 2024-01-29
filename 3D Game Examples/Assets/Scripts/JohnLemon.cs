32d1043468b36a997702ebc0345af1ebde86063e
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2024-01-29 12:29:21 -0800

    John Lemon MOvement

---
+++ 3D Game Examples/Assets/Scripts/JohnLemon.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JohnLemon : MonoBehaviour
+{
+    public float turnSpeed = 20f;
+
+    Animator m_Animator;
+    private Rigidbody _rigidbody;
+    private Vector3 _movement; //Member Value
+    public float movementSpeed;
+    Quaternion m_Rotation = Quaternion.identity;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+  
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        _movement.Set(horizontal, 0f, vertical);
+        _movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        //m_Animator.SetBool("isWalking", isWalking);
+        _rigidbody.MovePosition(_rigidbody.position + _movement * movementSpeed * Time.deltaTime);
+        _rigidbody.MoveRotation(m_Rotation);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, _movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    
+}
